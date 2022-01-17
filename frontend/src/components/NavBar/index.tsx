75d4f5fed149f4ddda37226bdd5c04cd22e57936
Marcos Miranda <mvms154@gmail.com>
2022-01-16 21:32:34 -0300

    Mundança na navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+          <a href="https://github.com/devsuperior">
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/devsuperior</p>
